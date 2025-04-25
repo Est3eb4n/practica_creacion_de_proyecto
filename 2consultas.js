a9a9ce4b31dc91418ada3ea2336e4182ad8553a5
Est3Est3eb4n <brice_esteban26@hotmail.com>
2025-04-25 08:27:26 -0500

    sd

--- 2consultas.js
+++ 2consultas.js
@@ -19,3 +19,11 @@ const limitResult = await collection.find({}).limit(3).toArray()
 
 // skip: omite un numero especificado de documentos al inicio de una consulta
 const skipResult = await collection.find({}).skip().toArray()
+
+// ejemplo de paginacion usando limit y skip
+
+page = 1;
+pageSize = 2;
+paginatedResult = await users.find({}).skip((page - 1)*pageSize).limit(pageSize).toArray();
+
+client.close();
